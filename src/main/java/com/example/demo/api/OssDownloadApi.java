cd77ec161bf7f7d934a89fc59da88a8e40aa4630
YinggangDong <believerD@aliyun.com>
2020-11-21 18:57:42 +0800

    style:添加注释

--- src/main/java/com/example/demo/api/OssDownloadApi.java
+++ src/main/java/com/example/demo/api/OssDownloadApi.java
@@ -3,7 +3,7 @@ package com.example.demo.api;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * OssDownloadApi 接口是
+ * OssDownloadApi 接口是 OSS服务器下载Api
  *
  * @author dongyinggang
  * @date 2020-11-11 19:50
