473db1830da266d3df7b9b5067ff34f27f2f8895
YinggangDong <believerD@aliyun.com>
2020-11-21 18:56:29 +0800

    style:添加注释

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,12 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * DemoApplication 类是 springboot启动类
+ *
+ * @author dongyinggang
+ * @date 2020/11/21 18:56
+ */
 @SpringBootApplication
 public class DemoApplication {
 
