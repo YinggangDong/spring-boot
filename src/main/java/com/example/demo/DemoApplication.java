0636e3238c43336225a9476533fb9917cab78f4b
YinggangDong <believerD@aliyun.com>
2020-11-25 17:51:24 +0800

    feat:修改并发demo内容

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * DemoApplication 类是 springboot启动类
+ * MapperScan 和 Mapper 必须得至少有一个,否则不会扫描到对应的 Mapper 对象
  *
  * @author dongyinggang
  * @date 2020/11/21 18:56
@@ -15,6 +16,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
     }
 
 }
--- src/main/java/com/example/demo/util/Demo.java
+++ src/main/java/com/example/demo/util/Demo.java
@@ -10,6 +10,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Demo 类是 CompletableFuture的demo测试
@@ -19,22 +20,25 @@ import java.util.concurrent.TimeUnit;
  **/
 @Slf4j
 public class Demo {
+
     public static void main(String[] args) {
+        test();
+    }
+    public static void test() {
+        AtomicInteger counter = new AtomicInteger(1);
 //        任务列表
         List<Integer> jobList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
         int nThreads = 3;
 //        工作线程池
         ExecutorService executorService = new ThreadPoolExecutor(nThreads, nThreads,
                 0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>());
+                new LinkedBlockingQueue<>(), (r) -> new Thread(r, "t-" + counter.getAndIncrement()));
 //        接收每个任务的执行结果
         List<CompletableFuture<String>> completableFutures = new ArrayList<>(jobList.size());
 
 //        任务分派
-        for (int i = 0; i < jobList.size(); i++) {
-
+        for (Integer jobId : jobList) {
             try {
-                Integer jobId = jobList.get(i);
 
                 CompletableFuture<String> integerCompletableFuture = CompletableFuture.supplyAsync(() -> {
 
@@ -47,30 +51,28 @@ public class Demo {
             } catch (Exception e) {
                 log.error(e.getMessage(), e);
             }
-
         }
 
         log.info("任务分派结束");
 
 //        等待全部任务执行完毕
-        CompletableFuture[] completableFutures1 = new CompletableFuture[completableFutures.size()];
+        CompletableFuture[] completableFutures1 = new CompletableFuture[0];
         CompletableFuture<Void> voidCompletableFuture =
                 CompletableFuture.allOf(completableFutures.toArray(completableFutures1));
         try {
             voidCompletableFuture.get();
             log.info("打印执行结果");
-            completableFutures.stream()
-                    .forEach(tmp -> {
-                        try {
-                            log.info("执行结果：{}", tmp.get());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    });
+            completableFutures.forEach(tmp -> {
+                try {
+                    log.info("执行结果：{}", tmp.get());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
         } catch (Exception e) {
             e.printStackTrace();
         }
-
+        voidCompletableFuture.join();
         executorService.shutdown();
         log.info("任务执行完毕");
 
