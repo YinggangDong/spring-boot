60fef6c826d8a6812c3dfecbcaa2bdceb211de77
YinggangDong <believerD@aliyun.com>
2022-05-16 14:28:19 +0800

    feature:测试feign日志组件相关的内容

---
+++ src/main/java/com/example/demo/feign/FeignDemoService.java
@@ -0,0 +1,17 @@
+package com.example.demo.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * FeignDemoService 类是
+ *
+ * @author dongyinggang
+ * @date 2022-05-16 13:34
+ **/
+@FeignClient(name = "feignName", url = "127.0.0.1:8080/")
+public interface FeignDemoService {
+
+    @GetMapping("/feign/hi")
+    String sayHi();
+}
---
+++ src/main/java/com/example/demo/util/socket/Dep.java
@@ -0,0 +1,15 @@
+package com.example.demo.util.socket;
+
+import lombok.Data;
+
+/**
+ * Dep 类是
+ *
+ * @author dongyinggang
+ * @date 2022-02-08 21:43
+ **/
+@Data
+public class Dep {
+
+    private String name;
+}
